b86fe21ad2ed6ba042e769320ac35307c23b695a
Taiki <yasutaka913@gmail.com>
2019-06-05 12:14:38 +0900

    完成

--- pan/pan_9.py
+++ pan/pan_9.py
@@ -1,6 +1,9 @@
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
 
-df=pd.read_csv('Testdata.csv')
-labels=['国語','数学','英語']
-print(labels)
+df = pd.read_csv('Testdata.csv')
+df['合計']=df[['国語','数学','英語']].sum(axis=1)
+df['順位']=df['合計'].rank(ascending=False)
+
+df.plot(kind='bar', x='ID', y='合計', rot=90)
+plt.show()
