d338407a2fbd6be313ca56fb73951b9a7cd8f5b4
yasut <yasut@192.168.10.8>
2019-06-21 19:38:42 +0900

    完成したなり

--- Hands-on/nigata2018_12.py
+++ Hands-on/nigata2018_12.py
@@ -1,18 +1,17 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.linear_model import  LinearRegression
 
 nig=pd.read_csv('niigata.csv',skiprows=[9])
 list_nig_x=[list(nig['1月']),list(nig['2月']),list(nig['3月']),list(nig['4月']),list(nig['5月']),list(nig['6月']),list(nig['7月']),list(nig['8月']),list(nig['9月']),list(nig['10月']),list(nig['11月'])]
 
-nig_y=pd.read_csv('ansniigata.csv').values.tolist()
+nig_y=pd.read_csv('ansniigata.csv')
+nig_y=list(nig_y['答'])
 
 joetsu=pd.read_csv('niigata.csv')
-sakata=joetsu.dropna()
-murakami=list(sakata['12月'])
+nagaoka=joetsu.dropna()
+murakami=[list(nagaoka['12月'])]
 
 lr=LinearRegression(normalize=True)
 lr.fit(list_nig_x,nig_y)
-ar_predict_y=lr.predict(murakami)
-print(ar_predict_y)
+shiwasu=lr.predict(murakami)
+print('平成最後の12月の予想気温は{0}'.format(shiwasu))
