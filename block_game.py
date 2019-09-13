3f8dbe58dca919a2c6e26647285995ea8089f23d
Taiki <yasutaka913@gmail.com>
2019-09-13 10:56:42 +0900

    追記

--- block_game.py
+++ block_game.py
@@ -56,6 +56,62 @@ def block_color(x, y):
 def drow_bar():
     cvs.delete("BAR")
     cvs.create_rectangle(bar_x - 80, bar_y - 12, bar_x + 80, bar_y + 12, fill="silver", width=0, tag="BAR")
+    cvs.create_rectangle(bar_x - 78, bar_y - 14, bar_x + 78, bar_y + 14, fill="silver", width=0, tag="BAR")
+    cvs.create_rectangle(bar_x - 78, bar_y - 12, bar_x + 78, bar_y + 12, fill="white", width=0, tag="BAR")
 
 
+def move_bar():
+    global bar_x
+    if key == "Left" and bar_x > 80:
+        bar_x = bar_x - 40
+    if key == "Right" and bar_x < 720:
+        bar_x = bar_x + 40
+
+
+def draw_ball():
+    cvs.delete("BALL")
+    cvs.create_oval(ball_x - 20, ball_y - 20, ball_x + 20, ball_y + 20, fill="gold", outline="orange", width=2,
+                    tag="BALL")
+    cvs.create_oval(ball_x - 16, ball_y - 16, ball_x + 12, ball_y + 12, fill="yellow",width=0,tag="BALL")
+
+def move_ball():
+    global idx,tmr,score,ball_x,ball_y,ball_xp,ball_yp
+    ball_x=ball_x+ball_xp
+    if ball_x<20:
+        ball_x=20
+        ball_xp=-ball_xp
+
+    if ball_x>780:
+        ball_x=780
+        ball_xp=-ball_xp
+    x=int(ball_x/80)
+    y=int(ball_y/40)
+    if block[y][x]==1:
+        block[y][x]=0
+        ball_xp=-ball_xp
+        score=score+10
+
+    ball_y=ball_y+ball_yp
+    if ball_y>=600:
+        idx=2
+        tmr=0
+        return
+    if ball_y<20:
+        ball_y=20
+        ball_yp=-ball_yp
+    x=int(ball_x/80)
+    y=int(ball_y/40)
+    if block[y][x]==1:
+        block[y][x]=0
+        ball_yp=-ball_yp
+        score=score+10
+
+    if bar_y-40<=ball_y and ball_y<=bar_y:
+        if bar_x-80<=ball_x and ball_x<=bar_x+80:
+            ball_yp=-10
+            score+score+1
+        elif bar_x-100<=ball_x and ball_x<=bar_x-80:
+            ball_yp=-10
+            ball_xp=random.randint(-20,-10)
+            score=score+2
 cvs = tkinter
